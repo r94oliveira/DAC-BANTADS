e8e42e0bb603b23a21f69ade4cd9f5f55bbeb983
luis19nn <luis19nn@gmail.com>
2022-12-14 19:18:02 -0300

    alterado tela inicial cliente para utilizar dados do json-server

--- front/src/app/cliente/home/home.component.ts
+++ front/src/app/cliente/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { LoginService } from 'src/app/auth/services/login.service';
+import { ContaService } from 'src/app/conta/services/conta.service';
+import { GerenteService } from 'src/app/gerente/services';
+import { Cliente, Conta, Gerente } from 'src/app/shared';
+import { ClienteService } from '../services';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +13,44 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  cliente: Cliente = new Cliente();
+  conta: Conta = new Conta();
+  gerente: Gerente = new Gerente(0);
 
-  ngOnInit(): void {}
+  constructor(
+    private clienteService: ClienteService,
+    private contaService: ContaService,
+    private gerenteService: GerenteService,
+    private loginService: LoginService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.clienteService
+      .buscarPorEmail(this.loginService.usuarioLogado.email)
+      .subscribe((cliente) => {
+        cliente = this.tratarRespostaSubscribe(cliente);
+        this.cliente = cliente;
+
+        this.contaService.buscarPorIdCliente(cliente.id).subscribe((conta) => {
+          conta = this.tratarRespostaSubscribe(conta);
+          this.conta = conta;
+
+          this.gerenteService
+            .buscarPorId(conta.idGerente)
+            .subscribe((gerente) => {
+              this.gerente = gerente;
+            });
+        });
+      });
+  }
+
+  tratarRespostaSubscribe(res: any) {
+    res = Object.values(res).reduce((a, b) => {
+      return a;
+    });
+
+    return res;
+  }
 }
