fe6d34246b222f4bc191e79510c3dab15631accb
leclm <lelima039@gmail.com>
2022-12-14 21:52:54 -0300

    Finaliza alterar-cliente

--- front/src/app/cliente/alterar/alterar.component.ts
+++ front/src/app/cliente/alterar/alterar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Cliente } from 'src/app/shared';
 import { ClienteService } from '../services/cliente.service';
+import { LoginService } from 'src/app/auth/services/login.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,6 +17,7 @@ export class AlterarComponent implements OnInit {
 
   constructor(
     private clienteService: ClienteService,
+    private loginService: LoginService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
@@ -25,14 +27,24 @@ export class AlterarComponent implements OnInit {
     // Operador + (antes do this) converte para número
     let id = +this.route.snapshot.params['id'];
 
-    // Com o id, obtém a pessoa
-    this.clienteService.buscarPorId(id).subscribe((res) => {
-      if (res) {
-        this.cliente = res;
+    this.clienteService
+    .buscarPorEmail(this.loginService.usuarioLogado.email)
+    .subscribe((cliente) => {
+      if (cliente) {
+        cliente = this.tratarRespostaSubscribe(cliente);
+        this.cliente = cliente;
       } else {
         throw new Error('Cliente não encontrado: id = ' + id);
       }
     });
+  }
+
+  tratarRespostaSubscribe(res: any) {
+    res = Object.values(res).reduce((a, b) => {
+      return a;
+    });
+
+    return res;
   }
 
   alterar(): void {
