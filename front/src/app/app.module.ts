c3270a3b3059546054268d0fbba2270d93797a0c
leclm <lelima039@gmail.com>
2022-12-19 19:06:00 -0300

    Ajusta máscara de valores em reais

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,35 +1,31 @@
-import { CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgSelectModule } from '@ng-select/ng-select';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { ClienteModule } from './cliente/cliente.module';
 import { GerenteModule } from './gerente/gerente.module';
-
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AuthModule } from './auth/auth.module';
-
 import { SharedModule } from './shared';
-
 import { NgxMaskModule, IConfig } from 'ngx-mask'
+import { CurrencyMaskConfig, CurrencyMaskModule, CURRENCY_MASK_CONFIG } from "ng2-currency-mask";
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
-import { CurrencyMaskConfig, CurrencyMaskModule } from "ng2-currency-mask";
 export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
   align: "right",
   allowNegative: true,
-  //allowZero: true,
   decimal: ",",
   precision: 2,
   prefix: "R$ ",
   suffix: "",
   thousands: "."
 };
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent],
@@ -48,8 +44,18 @@ export const CustomCurrencyMaskConfig: CurrencyMaskConfig = {
     CurrencyMaskModule
   ],
   providers: [
-    /*{ provide: LOCALE_ID,
-      useValue: 'pt-BR' } */
+    { provide: CURRENCY_MASK_CONFIG, useValue: CustomCurrencyMaskConfig },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+  },
+
+  /* if you don't provide the currency symbol in the pipe, 
+  this is going to be the default symbol (R$) ... */
+  {
+      provide:  DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+  },
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
